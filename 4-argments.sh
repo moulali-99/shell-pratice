d1813ee59fef3f03cb917feabd9d8d5e3d22693c
moulali pinjari <moulalipinjari92@gmail.com>
2026-02-04 23:31:20 +0530

    example

---
+++ 4-argments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: HI $PERSON2,GOOD MORNING"
+echo "$PERSON2:: HI $PERSON1,GOOD MORNING have nice day"
+echo "$PERSON1:: i am learning devops and happily and what abount you"
+echo "$PERSON2:: nothing $PERSON1 just preparing govt. exams and banks exams"
