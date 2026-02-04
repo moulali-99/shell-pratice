009899e8182a8848f5306c1c11f00a5f6dc86dd5
moulali pinjari <moulalipinjari92@gmail.com>
2026-02-04 23:28:56 +0530

    example

--- 3-example.sh
+++ 3-example.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-today=wednesday
+PERSON1=MOULALI
+PERSON2=PINJARI
 
-if(today !=sunday)
-{
-   echo "go to school" 
-}elif{
-  else
-       echo  "take holiday,enjoy"
-
-}
+echo "$PERSON1:: HI $PERSON2,GOOD MORNING"
+echo "$PERSON2:: HI $PERSON1,GOOD MORNING have nice day"
+echo "$PERSON1:: i am learning devops and happily and what abount you"
+echo "$PERSON2:: nothing $PERSON1 just preparing govt. exams and banks exams"
